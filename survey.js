3f6fb9e9f33a6b360f74103875a801c17703a016
jpared3s <jparedes.wall@gmail.com>
2023-01-17 18:17:02 -0500

    profile generator assignment

--- survey.js
+++ survey.js
@@ -14,17 +14,15 @@ const questions = [
 
 let idx = 0;
 const profileSurvey = () => {
-  if(idx < questions.length) {
-    rl.question(questions[idx], (answer) => { 
-      answers.push(answer)
-      idx += 1
+  if (idx < questions.length) {
+    rl.question(questions[idx], (answer) => {
+      answers.push(answer);
+      idx += 1;
       profileSurvey();
-    })
-    //if its less than two then we will ask a question, if its not the output answer
+    });
   } else {
-    console.log(`${answers[0]} loves to listen to ${answers[1]} while at the gym. He loves to watch ${answers[2]} in his spare time.`)
+    console.log(`${answers[0]} loves to listen to ${answers[1]} while at the gym. He loves to watch ${answers[2]} in his spare time.`);
     rl.close();
-    //output result
   }
 };
 profileSurvey();
